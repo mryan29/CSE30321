a7c0a1a879531a4987f317d55d2fa1cee24c13dd
Meaghan Elizabeth Ryan <mryan29@student00.cse.nd.edu>
2017-10-12 12:03:09 -0400

    Finished Problem A Part 4, code in no_blocking2.c

--- labs/02/Problem_A/no_blocking2.c
+++ labs/02/Problem_A/no_blocking2.c
@@ -1,15 +1,9 @@
-
-/* Version of no_blocking.c corresponding to step 4 of the lab prompt
-Step 4: unroll loop body such that 4 matrix elements calculated each iteration:
-		x[i][j], x[i+1][j], x[i][j+1], and x[i+1][j+1].
-*/
-
 #include <stdio.h>
 #include <math.h>
 
-#define XSIZE 3
-#define YSIZE 3
-#define MATRIX_SIZE 3
+#define XSIZE 100
+#define YSIZE 100
+#define MATRIX_SIZE 100
 int main(void) {
 
   int x[XSIZE][YSIZE];
@@ -20,58 +14,59 @@ int main(void) {
 
   int r;
 
-	printf("Initializing y matrix...\n");	// for testing purposes
-
+	printf("Initializing y matrix...\n");
   /* Initialize y matrix */
   for(i=0; i<XSIZE; i++) {
     for(j=0; j<YSIZE; j++) {
       y[i][j] = i + j;
-      printf("%d ", y[i][j]); // for testing purposes
+    //  printf("%d ", y[i][j]);
 
     }
-    printf("\n");				// for testing purposes
+    //printf("\n");
   }
 
-	printf("Initializing z matrix...\n");	// for testing purposes
+	printf("Initializing z matrix...\n");
   /* Initialize z matrix */
   for(i=0; i<XSIZE; i++) {
     for(j=0; j<YSIZE; j++) {
 	z[i][j] = i + j;
-	printf("%d ", z[i][j]);		// for testing purposes
+	//printf("%d ", z[i][j]);
     }
-    printf("\n");				// for testing purposes
+    //printf("\n");
   }
 
 	/* unrolled loop body such that 4 matrix elements calculated each iteration:
 		x[i][j], x[i+1][j], x[i][j+1], and x[i+1][j+1].
-	 */
-	printf("Building x matrix...\n");	// for testing purposes
-	// MATRIX_SIZE captures both the row and column dimensions of all matrices
-	for (i=0; i < MATRIX_SIZE - 1; i++) {
-		for (j=0; j < MATRIX_SIZE - 1; j++) {
+	*/
+	printf("Building x matrix...\n");
+	for (i=0; i < MATRIX_SIZE-1; i++) {
+		for (j=0; j < MATRIX_SIZE-1; j++) {
 			x[i][j] = 0;
 			x[i+1][j] = 0;
 			x[i][j+1] = 0;
 			x[i+1][j+1]=0;
 			for(k=0; k < MATRIX_SIZE; k++) {
-				x[i][j] 	= x[i][j] + y[i][k] * z[k][j];
+	//			printf("i: %d, j: %d, k: %d\n", i, j, k);
+				x[i][j] = x[i][j] + y[i][k] * z[k][j];
+	//			printf("x[i][j]: %d\t", x[i][j]);
 				x[i+1][j] 	= x[i+1][j] + y[i+1][k] * z[k][j];
+	//			printf("x[i+1][j]: %d\t", x[i+1][j]);
 				x[i][j+1]	= x[i][j+1] + y[i][k] * z[k][j+1];
+	//			printf("x[i][j+1]: %d\t", x[i][j+1]);
 				x[i+1][j+1]	= x[i+1][j+1] + y[i+1][k] * z[k][j+1];
+	//			printf("x[i+1][j+1]: %d\n", x[i+1][j+1]);
 			}
 			j++;
-			//printf("%d ", x[i][j]);	// for testing purposes
 		}
 		i++;
-//	printf("\n");				// for testing purposes
 	}
 
 	// for testing purposes
-	for (i = 0; i < MATRIX_SIZE; i++) {
+/*	for (i = 0; i < MATRIX_SIZE; i++) {
 		for (j = 0; j < MATRIX_SIZE; j++) {
 			printf("%d ", x[i][j]);
 		}
 		printf("\n");
 	}
-
+*/
 }
