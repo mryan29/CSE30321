dfbaa682cd8c6b57e202ff794e02710bb901e140
Alanna Lynn McEachen <amceache@student04.cse.nd.edu>
2017-11-16 23:23:49 -0500

    lab3 stuff

---
+++ labs/03/blocking.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <math.h>
+
+#define MATRIX_SIZE 5 
+#define XSIZE 5 
+#define YSIZE 5
+
+int main(void) {
+
+  int x[XSIZE][YSIZE];
+  int y[XSIZE][YSIZE];
+  int z[XSIZE][YSIZE];
+
+  int i, j, k;
+  int B = 1;
+  
+  int r;
+
+  /* Initialize x matrix */
+  for(i=0; i<XSIZE; i++) {
+    for(j=0; j<YSIZE; j++) {
+      x[i][j] = 0;
+    }
+  }
+
+  /* Initialize y matrix */
+  for(i=0; i<XSIZE; i++) {
+    for(j=0; j<YSIZE; j++) {
+      y[i][j] = i + j;
+    }
+  }
+
+  /* Initialize z matrix */
+  for(i=0; i<XSIZE; i++) {
+    for(j=0; j<YSIZE; j++) {
+	z[i][j] = i + j;
+    }
+  }
+
+  /* Do matrix multiply */
+
+/*
+  for(i=0; i<XSIZE; i=i+1) {
+    for(j=0; j<YSIZE; j=j+1) {
+      r = 0;
+      for(k=0; k<XSIZE; k=k+1) {
+        r = r + y[i][k] * z[k][j];
+      }
+      x[i][j] = r;
+      printf("%i ", r);
+    }
+    printf("\n");
+  }
+*/
+
+// i originally had i++, j++, etc instead of i+b
+// but i think you have to expand the code more than this (which is essentially
+// what was given)
+// I was using this link for reference before running out of time
+// http://csapp.cs.cmu.edu/2e/waside/waside-blocking.pdf
+
+  for (i=0; i < MATRIX_SIZE; i=i+B) {
+    for (j=0; j < MATRIX_SIZE; j=j+B) {
+      for (k=0; k < MATRIX_SIZE; k=k+B) {
+        x[i][j] = x[i][j] + y[i][k] * z[k][j];
+      }
+      printf("%i ", x[i][j]);
+    }
+    printf("\n");
+  }
+}
---
+++ labs/03/no_blocking.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <math.h>
+
+#define XSIZE 5
+#define YSIZE 5
+
+int main(void) {
+
+  int x[XSIZE][YSIZE];
+  int y[XSIZE][YSIZE];
+  int z[XSIZE][YSIZE];
+
+  int i, j, k;
+  
+  int r;
+
+  /* Initialize x matrix */
+  for(i=0; i<XSIZE; i++) {
+    for(j=0; j<YSIZE; j++) {
+      x[i][j] = 0;
+    }
+  }
+
+  /* Initialize y matrix */
+  for(i=0; i<XSIZE; i++) {
+    for(j=0; j<YSIZE; j++) {
+      y[i][j] = i + j;
+    }
+  }
+
+  /* Initialize z matrix */
+  for(i=0; i<XSIZE; i++) {
+    for(j=0; j<YSIZE; j++) {
+	z[i][j] = i + j;
+    }
+  }
+
+  /* Do matrix multiply */
+  for(i=0; i<XSIZE; i=i+1) {
+    for(j=0; j<YSIZE; j=j+1) {
+      r = 0;
+      for(k=0; k<XSIZE; k=k+1) {
+        r = r + y[i][k] * z[k][j];
+      }
+      x[i][j] = r;
+      printf("%i ", r);
+    }
+    printf("\n");
+  }
+}
